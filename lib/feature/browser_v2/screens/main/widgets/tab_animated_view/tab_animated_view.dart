f391ec37c95cce2f70603a8927faab487ebb2465
knightforce <andrianov44@gmail.com>
2025-03-23 20:09:21 +0700

    Browser select tab. Animate from start view

--- lib/feature/browser_v2/screens/main/widgets/tab_animated_view/tab_animated_view.dart
+++ lib/feature/browser_v2/screens/main/widgets/tab_animated_view/tab_animated_view.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:app/feature/browser_v2/screens/main/widgets/browser_start_view.dart';
 import 'package:app/feature/browser_v2/screens/main/widgets/tab_animated_view/tab_animated_view_wm.dart';
 import 'package:app/feature/browser_v2/screens/main/widgets/tab_animated_view/tab_animation_type.dart';
+import 'package:app/generated/generated.dart';
 import 'package:elementary/elementary.dart';
 import 'package:elementary_helper/elementary_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_components_lib/v2/ui_components_lib_v2.dart';
 
 class TabAnimatedView extends ElementaryWidget<TabAnimatedViewWidgetModel> {
   const TabAnimatedView({
@@ -59,7 +62,7 @@ class TabAnimatedView extends ElementaryWidget<TabAnimatedViewWidgetModel> {
                     wm.borderRadiusAnimation.value,
                   ),
                   child: file == null
-                      ? Container(color: Colors.blue)
+                      ? const _StartPageAnimationView()
                       : Image.file(
                           file,
                           fit: BoxFit.fill,
@@ -70,6 +73,33 @@ class TabAnimatedView extends ElementaryWidget<TabAnimatedViewWidgetModel> {
           ),
         );
       },
+    );
+  }
+}
+
+class _StartPageAnimationView extends StatelessWidget {
+  const _StartPageAnimationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.themeStyleV2;
+    final styles = theme.textStyles;
+    final colors = theme.colors;
+
+    return SizedBox(
+      height: double.infinity,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: context.themeStyleV2.colors.background0,
+        ),
+        child: Transform.translate(
+          offset: const Offset(0, -DimensSizeV2.d140),
+          child: Assets.images.bgNetwork.image(
+            width: double.infinity,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+      ),
     );
   }
 }
