91946b8b0345c76d19ac0999aa6ea76a19d50da5
knightforce <andrianov44@gmail.com>
2025-03-28 22:58:04 +0700

    Browser pages list background

--- lib/feature/browser_v2/screens/main/widgets/pages_view/pages_view.dart
+++ lib/feature/browser_v2/screens/main/widgets/pages_view/pages_view.dart
@@ -3,6 +3,8 @@ import 'package:app/feature/browser_v2/screens/main/widgets/page/browser_page.da
 import 'package:elementary_helper/elementary_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:ui_components_lib/components/button/mini_button.dart';
+import 'package:ui_components_lib/v2/ui_components_lib_v2.dart';
 
 class BrowserPagesView extends StatelessWidget {
   const BrowserPagesView({
@@ -39,34 +41,37 @@ class BrowserPagesView extends StatelessWidget {
           maintainState: true,
           maintainSize: true,
           maintainAnimation: true,
-          child: StateNotifierBuilder<BrowserTabsCollection?>(
-            listenableState: tabsState,
-            builder: (_, BrowserTabsCollection? data) {
-              if (data == null) {
-                return const SizedBox.shrink();
-              }
+          child: ColoredBox(
+            color: context.themeStyleV2.colors.background1,
+            child: StateNotifierBuilder<BrowserTabsCollection?>(
+              listenableState: tabsState,
+              builder: (_, BrowserTabsCollection? data) {
+                if (data == null) {
+                  return const SizedBox.shrink();
+                }
 
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                controller: scrollController,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: data.count,
-                itemBuilder: (_, int index) {
-                  return BrowserPage(
-                    key: ValueKey(data.list[index].id),
-                    progressController: progressController,
-                    width: width,
-                    tab: data.list[index],
-                    onCreate: (controller) => onCreateWebViewController(
-                      data.list[index].id,
-                      controller,
-                    ),
-                    onScrollChanged: onScrollChanged,
-                    onDispose: () => onDispose(data.list[index].id),
-                  );
-                },
-              );
-            },
+                return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  controller: scrollController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: data.count,
+                  itemBuilder: (_, int index) {
+                    return BrowserPage(
+                      key: ValueKey(data.list[index].id),
+                      progressController: progressController,
+                      width: width,
+                      tab: data.list[index],
+                      onCreate: (controller) => onCreateWebViewController(
+                        data.list[index].id,
+                        controller,
+                      ),
+                      onScrollChanged: onScrollChanged,
+                      onDispose: () => onDispose(data.list[index].id),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         );
       },
