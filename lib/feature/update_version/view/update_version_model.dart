d29a08eee653fe4182ffe3c3dd081e925fb5fa45
levitckii-daniil <me@levitckiidaniil.ru>
2025-03-31 09:13:09 +0300

    feat(EWM-492): android in-app update support

--- lib/feature/update_version/view/update_version_model.dart
+++ lib/feature/update_version/view/update_version_model.dart
@@ -1,8 +1,13 @@
+import 'package:app/bootstrap/sentry.dart';
 import 'package:app/feature/update_version/data/update_request.dart';
+import 'package:app/feature/update_version/data/update_status.dart';
 import 'package:app/feature/update_version/domain/store_url_generator.dart';
 import 'package:app/feature/update_version/domain/update_service.dart';
 import 'package:elementary/elementary.dart';
+import 'package:flutter/foundation.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:logging/logging.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class UpdateVersionModel extends ElementaryModel {
@@ -10,13 +15,22 @@ class UpdateVersionModel extends ElementaryModel {
     ErrorHandler errorHandler,
     this._updateService,
     this._storeUrlGenerator,
+    this._sentry,
   ) : super(errorHandler: errorHandler);
 
   final UpdateService _updateService;
   final StoreUrlGenerator _storeUrlGenerator;
+  final SentryWorker _sentry;
+
   final _logger = Logger('UpdateVersionModel');
 
-  Stream<UpdateRequest?> get updateRequests => _updateService.updateRequests;
+  ValueStream<UpdateRequest?> get updateRequests =>
+      _updateService.updateRequests;
+
+  Future<String> get storeUrl => _storeUrlGenerator.storeUrl();
+
+  bool get isCloseAvailable =>
+      updateRequests.value?.status != UpdateStatus.blocking;
 
   Future<void> dismissWarning() async {
     await _updateService.dismissWarning();
@@ -25,12 +39,51 @@ class UpdateVersionModel extends ElementaryModel {
   /// Open the update URL in the app store
   Future<void> openAppStore() async {
     try {
-      final storeUrl = await _storeUrlGenerator.storeUrl();
-      final url = Uri.parse(storeUrl);
-      await launchUrl(url, mode: LaunchMode.externalApplication);
+      final isInAppFlowSuccess = await _inAppUpdateFlow();
+      if (!isInAppFlowSuccess) {
+        final storeUrl = await _storeUrlGenerator.storeUrl();
+        final url = Uri.parse(storeUrl);
+        await launchUrl(url, mode: LaunchMode.externalApplication);
+      }
     } catch (e, s) {
       handleError(e, stackTrace: s);
-      _logger.severe('Error launching URL', e, s);
+
+      _sentry.captureException(e, stackTrace: s);
+    }
+
+    await dismissWarning();
+  }
+
+  Future<bool> _inAppUpdateFlow() async {
+    if (defaultTargetPlatform != TargetPlatform.android) return false;
+
+    try {
+      final updateInfo = await InAppUpdate.checkForUpdate();
+      if (updateInfo.updateAvailability ==
+          UpdateAvailability.updateNotAvailable) {
+        _logger.warning('In app update not available $updateInfo');
+        return false;
+      }
+
+      if (!updateInfo.immediateUpdateAllowed) {
+        _logger.warning('In app update not available $updateInfo');
+        return false;
+      }
+
+      final immediateUpdateStatus = await InAppUpdate.performImmediateUpdate();
+
+      if (immediateUpdateStatus == AppUpdateResult.inAppUpdateFailed) {
+        _logger.warning('In app update failed  $updateInfo');
+        return false;
+      }
+
+      _logger.info('In app update finised $updateInfo');
+      return true;
+    } catch (e, s) {
+      _logger.severe('Error catch during in app update', e, s);
+      _sentry.captureException(e, stackTrace: s);
+
+      return false;
     }
   }
 }
