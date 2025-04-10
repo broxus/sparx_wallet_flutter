a5b40987f00dd406203f3df5d3ce7146314e139c
knightforce <andrianov44@gmail.com>
2025-04-11 02:20:31 +0700

    Browser. BrowserBookmarksStorageService clear use tryWrapper and log

--- lib/feature/browser_v2/domain/service/storages/browser_bookmarks_storage_service.dart
+++ lib/feature/browser_v2/domain/service/storages/browser_bookmarks_storage_service.dart
@@ -1,7 +1,10 @@
 import 'package:app/app/service/service.dart';
+import 'package:app/bootstrap/sentry.dart';
 import 'package:app/data/models/models.dart';
+import 'package:app/utils/common_utils.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:logging/logging.dart';
 
 const _browserBookmarksDomain = 'browser_bookmarks';
 const _browserBookmarksKey = 'browser_bookmarks_key';
@@ -17,6 +20,10 @@ class BrowserBookmarksStorageService extends AbstractStorageService {
   static const container = _browserBookmarksDomain;
 
   final GetStorage _storage;
+
+  final _sentry = SentryWorker.instance;
+
+  final _log = Logger('BrowserBookmarksStorageService');
 
   /// Read list of browser bookmarks items from storage
   List<BrowserBookmarkItem> getBrowserBookmarks() {
@@ -48,8 +55,17 @@ class BrowserBookmarksStorageService extends AbstractStorageService {
 
   @override
   Future<void> clear() async {
-    try {
-      return await _storage.erase();
-    } catch (_) {}
+    await tryWrapper(
+      _storage.erase,
+      onCatch: _captureException,
+    );
+  }
+
+  Future<void> _captureException(Object e, StackTrace s) async {
+    _log.severe('BrowserBookmarksStorageService captureException', e, s);
+    _sentry.captureException(
+      e,
+      stackTrace: s,
+    );
   }
 }
