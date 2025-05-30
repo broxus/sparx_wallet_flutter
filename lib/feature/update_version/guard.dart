a20c4ccd7bf0ac7cde0b46e8e4bf1a7497d91f9c
levitckii-daniil <me@levitckiidaniil.ru>
2025-05-30 17:34:20 +0300

    fix(EWM_TECH): Fixed bug, when currentRoutesStream notify listeners with same route after compassPush (it will make recursive update bug)

--- lib/feature/update_version/guard.dart
+++ lib/feature/update_version/guard.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'package:app/app/router/compass/compass.dart';
 import 'package:app/app/router/router.dart';
+import 'package:app/bootstrap/sentry.dart';
 import 'package:app/feature/update_version/route.dart';
 import 'package:app/feature/update_version/update_version.dart';
 import 'package:app/feature/wallet/route.dart';
+import 'package:app/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:logging/logging.dart';
@@ -18,12 +20,15 @@ class UpdateVersionGuard extends CompassGuard {
     this._updateService,
   ) : super(priority: priorityLow);
 
+  static const _minimumDelaySeconds = 60;
+
   final UpdateService _updateService;
   final _log = Logger('UpdateVersionGuard');
 
   StreamSubscription<(UpdateRequest?, CompassBaseGoRoute?)>?
       _updateVersionSubscription;
   CompassRouter? _router;
+  int? _warningLastTimeSecs;
 
   @override
   void attachToRouter(CompassRouter router) {
@@ -52,18 +57,49 @@ class UpdateVersionGuard extends CompassGuard {
     return null;
   }
 
-  void _onUpdateRequests(
+  Future<void> _onUpdateRequests(
     (UpdateRequest?, CompassBaseGoRoute?) requestRoute,
-  ) {
+  ) async {
     final (request, currentRoute) = requestRoute;
     if (request == null) return;
     if (currentRoute is! WalletRoute) return;
 
     _log.info('Open update version screen $currentRoute, $request');
 
-    _router?.compassPush(
+    if (!_checkMinimumDelayBetweenShow()) {
+      return;
+    }
+
+    await _router?.compassPush(
       const UpdateVersionRouteData(),
       isContinue: false,
     );
+  }
+
+  // Normally this check isn’t required, but if a bug causes
+  // `UpdateVersionRouteData()` to fire twice, it can lead to a jarring user
+  // experience. This method enforces a minimum delay between shows.
+  bool _checkMinimumDelayBetweenShow() {
+    final nowSecs = NtpTime.now().millisecondsSinceEpoch ~/ 1000;
+
+    final warningLastTimeSecs = _warningLastTimeSecs;
+    if (warningLastTimeSecs != null) {
+      final elapsedSecs = nowSecs - warningLastTimeSecs;
+
+      if (elapsedSecs < _minimumDelaySeconds) {
+        _log.info(
+          'Not enough time between request show: '
+          '$elapsedSecs < $_minimumDelaySeconds',
+        );
+        SentryWorker.instance.captureException(
+          StateError('Update screen tries show twice'),
+        );
+        return false;
+      }
+    }
+
+    _warningLastTimeSecs = nowSecs;
+
+    return true;
   }
 }
