611d32f54512c08394c9500f615c270dfacd8955
knightforce <andrianov44@gmail.com>
2025-06-03 10:20:34 +0700

    EWM-523 NotNullNotifier description comment

--- lib/core/wm/not_null_safe_notifier.dart
+++ lib/core/wm/not_null_safe_notifier.dart
@@ -1,6 +1,11 @@
 import 'package:app/core/wm/not_null_listenable_state.dart';
 import 'package:flutter/cupertino.dart';
 
+/// Successor of ChangeNotifier and Listenable.
+/// Analog of SafeStateNotifier, but accepting only non-null values.
+///
+/// Ensures that the value is not null.
+/// There is no need to write additional null checks when working with value.
 class NotNullNotifier<T> extends ChangeNotifier
     implements NotNullListenableState<T> {
   NotNullNotifier(T initValue) : _value = initValue;
