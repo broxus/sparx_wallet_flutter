a940d7ca162039e8951cb57471d0cd75490cdf14
knightforce <andrianov44@gmail.com>
2025-08-07 01:39:35 +0700

    fix(414): AccountSettingsSheet by Elementary

---
+++ lib/feature/profile/key_detail/widgets/account_settings_sheet/account_settings_sheet_model.dart
@@ -0,0 +1,47 @@
+import 'package:app/feature/browser_v2/domain/browser_launcher.dart';
+import 'package:app/feature/messenger/data/message.dart';
+import 'package:app/feature/messenger/domain/service/messenger_service.dart';
+import 'package:app/generated/generated.dart';
+import 'package:elementary/elementary.dart';
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
+import 'package:nekoton_repository/nekoton_repository.dart' hide Message;
+
+/// [ElementaryModel] for [AccountSettingsSheet]
+@injectable
+class AccountSettingsSheetModel extends ElementaryModel {
+  AccountSettingsSheetModel(
+    ErrorHandler errorHandler,
+    this._nekotonRepository,
+    this._messengerService,
+    this._browserLauncher,
+  ) : super(errorHandler: errorHandler);
+
+  final NekotonRepository _nekotonRepository;
+  final MessengerService _messengerService;
+  final BrowserLauncher _browserLauncher;
+
+  KeyAccount? getAccount(Address address) =>
+      _nekotonRepository.seedList.findAccountByAddress(address);
+
+  void copyAddress(Address address) {
+    Clipboard.setData(
+      ClipboardData(text: address.address),
+    );
+    _messengerService.show(
+      Message.successful(
+        message: LocaleKeys.valueCopiedExclamation.tr(
+          args: [address.toEllipseString()],
+        ),
+      ),
+    );
+  }
+
+  void openBrowserByString(Address address) {
+    _browserLauncher.openBrowserByString(
+      _nekotonRepository
+          .currentTransport
+          .accountExplorerLink(address),
+    );
+  }
+}
