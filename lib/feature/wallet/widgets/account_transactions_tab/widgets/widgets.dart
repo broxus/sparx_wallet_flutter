8db42f24202e78f2ab89314e41264ef6f4256f77
knightforce <andrianov44@gmail.com>
2025-08-07 16:59:48 +0700

    fix(414): TonWalletExpiredTransactionWidget by Elementary

--- lib/feature/wallet/widgets/account_transactions_tab/widgets/widgets.dart
+++ lib/feature/wallet/widgets/account_transactions_tab/widgets/widgets.dart
@@ -1,5 +1,5 @@
 export 'scroll_controller_preload_listener.dart';
-export 'ton_wallet_expired_transaction_widget.dart';
+export 'ton_wallet_expired_transaction_widget/ton_wallet_expired_transaction_widget.dart';
 export 'ton_wallet_multisig_expired_transaction/ton_wallet_multisig_expired_transaction_widget.dart';
 export 'ton_wallet_multisig_ordinary_transaction_widget/ton_wallet_multisig_ordinary_transaction_widget.dart';
 export 'ton_wallet_multisig_pending_transaction_widget/ton_wallet_multisig_pending_transaction_widget.dart';
