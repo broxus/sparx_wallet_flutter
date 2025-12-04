63331619720595c9b68cbeab9760db669cbb1dd0
knightforce <andrianov44@gmail.com>
2025-12-04 14:27:41 +0500

    EWM-599: showLogOutConfirmSheet fix import

--- lib/feature/profile/widgets/logout_confirm_sheet.dart
+++ lib/feature/profile/widgets/logout_confirm_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:app/generated/locale_keys.g.dart';
-import 'package:app/widgets/bottom_space.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
