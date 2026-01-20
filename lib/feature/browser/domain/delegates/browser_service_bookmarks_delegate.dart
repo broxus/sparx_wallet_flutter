b9de12912055e87316f08c0e4fb6458a0fbf1602
knightforce <andrianov44@gmail.com>
2026-01-20 17:13:13 +0700

    EWM-626: remove TODO from BrowserServiceBookmarksDelegate.reorder

--- lib/feature/browser/domain/delegates/browser_service_bookmarks_delegate.dart
+++ lib/feature/browser/domain/delegates/browser_service_bookmarks_delegate.dart
@@ -48,12 +48,6 @@ class BrowserServiceBookmarksDelegate
   List<BrowserBookmarkItem> get browserBookmarks =>
       _browserBookmarksSubject.valueOrNull ?? [];
 
-  // TODO(knightsforce): check need it?
-  List<BrowserBookmarkItem> get sortedBookmarks {
-    return [...browserBookmarks]
-      ..sort((a, b) => (b.sortingOrder - a.sortingOrder).sign.toInt());
-  }
-
   void init() {
     _fetchBookmarksFromStorage();
   }
@@ -118,9 +112,10 @@ class BrowserServiceBookmarksDelegate
     if (index > bookmarks.length) {
       index = bookmarks.length;
     }
-
-    // TODO(knightforce): hack for:
-    // https://github.com/flutter/flutter/issues/56821
+    // Hack for:
+    // https://github.com/flutter/flutter/issues/24786
+    // Due to backward compatibility issues,
+    // the development team decided not to fix this issue.
     if (oldIndex < index) {
       index--;
     }
