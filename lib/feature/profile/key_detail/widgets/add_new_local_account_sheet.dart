4fd065a57775c80f170ee7c69643288d83036df4
knightforce <andrianov44@gmail.com>
2026-01-23 14:57:00 +0700

    EWM-636: add_new_local_account_sheet.dart remove unused import

--- lib/feature/profile/key_detail/widgets/add_new_local_account_sheet.dart
+++ lib/feature/profile/key_detail/widgets/add_new_local_account_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:app/feature/profile/profile.dart';
 import 'package:app/generated/generated.dart';
 import 'package:app/utils/constants.dart';
-import 'package:convert/convert.dart';
 import 'package:flutter/material.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
