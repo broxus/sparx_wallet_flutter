00e51e8cc504474064b3ade356f346a840ea517d
knightforce <andrianov44@gmail.com>
2026-01-26 01:11:48 +0700

    EWM-637: select_account_sheet.dart remove unused import

--- lib/feature/wallet/widgets/select_account/select_account_sheet.dart
+++ lib/feature/wallet/widgets/select_account/select_account_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:app/feature/wallet/widgets/select_account/select_account_widget.dart';
-import 'package:app/generated/generated.dart';
 import 'package:flutter/material.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
