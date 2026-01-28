0095e98abfaf9da78d0041fb11c7227f57f1ba4a
knightforce <andrianov44@gmail.com>
2026-01-28 15:38:06 +0700

    EWM-637: Remove import

--- lib/feature/wallet/widgets/select_account/layouts/select_account_widget_default_layout.dart
+++ lib/feature/wallet/widgets/select_account/layouts/select_account_widget_default_layout.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
-import 'package:ui_components_lib/v2/ui_components_lib_v2.dart';
 
 class SelectAccountWidgetDefaultLayout extends StatelessWidget {
   const SelectAccountWidgetDefaultLayout(
