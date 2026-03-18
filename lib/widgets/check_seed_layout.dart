7de8b2467d4705c670af998379dd5fc6c1d660ca
knightforce <andrianov44@gmail.com>
2026-03-18 16:21:40 +0700

    feat: EWM-670 check_seed_layout.dart ProtectedContent

--- lib/widgets/check_seed_layout.dart
+++ lib/widgets/check_seed_layout.dart
@@ -2,6 +2,7 @@ import 'package:app/feature/add_seed/check_seed_phrase/data/check_seed_correct_a
 import 'package:app/feature/add_seed/check_seed_phrase/widgets/check_seed_answers_widget.dart';
 import 'package:app/feature/add_seed/check_seed_phrase/widgets/check_seed_available_answers_widget.dart';
 import 'package:app/generated/generated.dart';
+import 'package:app/widgets/protected_content.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
 
@@ -32,44 +33,46 @@ class CheckSeedLayout extends StatelessWidget {
         .where((e) => e.isNotEmpty)
         .toList();
 
-    return Column(
-      children: [
-        Image.asset(
-          Assets.images.circleCheck.path,
-          width: DimensSize.d56,
-          height: DimensSize.d56,
-        ),
-        const SizedBox(height: DimensSize.d16),
-        Text(
-          LocaleKeys.letsCheckSeedPhrase.tr(),
-          style: textStyles.headingLarge,
-        ),
-        const SizedBox(height: DimensSize.d8),
-        Text(
-          LocaleKeys.checkSeedPhraseCorrectly.tr(),
-          style: textStyles.paragraphMedium,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: DimensSize.d24),
-        CheckSeedAnswersWidget(
-          userAnswers: userAnswers,
-          currentIndex: currentCheckIndex,
-          clearAnswer: clearAnswer,
-        ),
-        const SizedBox(height: DimensSize.d24),
-        CheckSeedAnswersCounterWidget(
-          currentIndex: words.length,
-          count: userAnswers.length,
-        ),
-        const SizedBox(height: DimensSize.d24),
-        const Spacer(),
-        CheckSeedAvailableAnswersWidget(
-          availableAnswers: availableAnswers,
-          selectedAnswers: words,
-          selectAnswer: answerQuestion,
-        ),
-        const SizedBox(height: DimensSize.d16),
-      ],
+    return ProtectedContent(
+      child: Column(
+        children: [
+          Image.asset(
+            Assets.images.circleCheck.path,
+            width: DimensSize.d56,
+            height: DimensSize.d56,
+          ),
+          const SizedBox(height: DimensSize.d16),
+          Text(
+            LocaleKeys.letsCheckSeedPhrase.tr(),
+            style: textStyles.headingLarge,
+          ),
+          const SizedBox(height: DimensSize.d8),
+          Text(
+            LocaleKeys.checkSeedPhraseCorrectly.tr(),
+            style: textStyles.paragraphMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: DimensSize.d24),
+          CheckSeedAnswersWidget(
+            userAnswers: userAnswers,
+            currentIndex: currentCheckIndex,
+            clearAnswer: clearAnswer,
+          ),
+          const SizedBox(height: DimensSize.d24),
+          CheckSeedAnswersCounterWidget(
+            currentIndex: words.length,
+            count: userAnswers.length,
+          ),
+          const SizedBox(height: DimensSize.d24),
+          const Spacer(),
+          CheckSeedAvailableAnswersWidget(
+            availableAnswers: availableAnswers,
+            selectedAnswers: words,
+            selectAnswer: answerQuestion,
+          ),
+          const SizedBox(height: DimensSize.d16),
+        ],
+      ),
     );
   }
 }
